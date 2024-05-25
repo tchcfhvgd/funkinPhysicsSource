753530d6d25927402c38aceb4793846180dd2bc1
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-25 14:40:03 +0800

    Update FlxTransWindow.hx

--- source/FlxTransWindow.hx
+++ source/FlxTransWindow.hx
@@ -1,5 +1,6 @@
 package;
 
+#if desktop
 @:cppFileCode('#include <windows.h>\n#include <dwmapi.h>\n\n#pragma comment(lib, "Dwmapi")')
 class FlxTransWindow
 {
@@ -29,3 +30,4 @@ class FlxTransWindow
 		return res;
 	}
 }
+#end
